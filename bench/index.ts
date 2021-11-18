967352cb33be06e71145f9741cc385026aa73087
Marais Rossouw <me@marais.co>
2021-11-18 17:20:18 +1000

    chore: adds benchmarks

---
+++ bench/index.ts
@@ -0,0 +1,79 @@
+import { Suite } from 'benchmark';
+import * as assert from 'uvu/assert';
+
+import * as trcprnt from '../src';
+import TraceParent from 'traceparent';
+
+import { webcrypto, randomBytes } from 'node:crypto';
+
+// @ts-ignore
+globalThis.crypto = webcrypto;
+
+function runner(
+	name: string,
+	candidates: Record<string, Function>,
+	valid: (s: string) => boolean,
+) {
+	console.log('\nValidation :: %s', name);
+	Object.keys(candidates).forEach((name) => {
+		const result = candidates[name]();
+		try {
+			assert.ok(valid(result), `${result} is not ok`);
+			console.log(`✔`, name);
+		} catch (err) {
+			console.log('✘', name, `(FAILED @ "${err.message}")`);
+		}
+	});
+
+	console.log('\nBenchmark :: %s', name);
+	const bench = new Suite().on('cycle', (e) => console.log('  ' + e.target));
+	Object.keys(candidates).forEach((name) => {
+		bench.add(name + ' '.repeat(22 - name.length), () =>
+			candidates[name](),
+		);
+	});
+
+	bench.run();
+}
+
+runner(
+	'make',
+	{
+		trcprnt: () => String(trcprnt.make()),
+		TraceParent: () => {
+			const version = Buffer.alloc(1).toString('hex');
+			const traceId = randomBytes(16).toString('hex');
+			const id = randomBytes(8).toString('hex');
+			const flags = '01';
+
+			return String(
+				TraceParent.fromString(`${version}-${traceId}-${id}-${flags}`),
+			);
+		},
+	},
+	(o: string) =>
+		/^((?![f]{2})[a-f0-9]{2})-((?![0]{32})[a-f0-9]{32})-((?![0]{16})[a-f0-9]{16})-([a-f0-9]{2})$/.test(
+			o,
+		),
+);
+
+runner(
+	'parse',
+	{
+		trcprnt: () =>
+			String(
+				trcprnt.parse(
+					'00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-01',
+				),
+			),
+		TraceParent: () => {
+			return String(
+				TraceParent.fromString(
+					'00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-01',
+				),
+			);
+		},
+	},
+	(o: string) =>
+		'00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-01' === o,
+);
---
+++ readme.md
@@ -0,0 +1,9 @@
+Validation :: make ✔ trcprnt ✔ TraceParent
+
+Benchmark :: make trcprnt x 94,483 ops/sec ±6.83% (81 runs sampled) TraceParent x 50,670 ops/sec ±5.58% (79 runs
+sampled)
+
+Validation :: parse ✔ trcprnt ✔ TraceParent
+
+Benchmark :: parse trcprnt x 172,333 ops/sec ±1.16% (93 runs sampled) TraceParent x 121,451 ops/sec ±4.17% (79 runs
+sampled)
