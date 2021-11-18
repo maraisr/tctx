ace407732f0bef6054be630bbc9298ce7fecc164
Tibo Delor <tibo.delor@zip.co>
2021-11-18 21:51:54 +1100

    Replace buff with strings

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { toHEX, asHEX } from '#hex';
+import { toHEX } from '#hex';
 import { fill_random } from '#crypto';
 
 export interface Traceparent {
@@ -22,56 +22,43 @@ export interface Traceparent {
  version (2 hex)
 */
 
-const v_size = 1,
-	t_size = 16,
-	p_size = 8;
 
-const v_offset = 0,
-	t_offset = v_size,
-	p_offset = t_offset + t_size,
-	f_offset = p_offset + p_size;
+const trace_id_size = 16,
+	parent_id_size = 8;
+const default_version = "00";
+const sampled_flag = "00";
 
-/*#__INLINE__*/
-const slice = (buf: Uint8Array, offset: number, end?: number): string =>
-	toHEX(buf.slice(offset, end));
 
-const traceparent = (buf: Uint8Array) => ({
-	version: slice(buf, v_offset, t_offset),
-	trace_id: slice(buf, t_offset, p_offset),
-	parent_id: slice(buf, p_offset, f_offset),
-	// TODO: flags should be binary 0b00000000
-	flags: slice(buf, f_offset),
+const traceparent = (version:string, trace_id:string, parent_id: string, flags:string): Traceparent => ({
+	version,
+	trace_id,
+	parent_id,
+	flags,
 
 	child() {
-		const new_buf = new Uint8Array(buf);
-		fill_random(new_buf, p_offset, p_size);
-		return traceparent(new_buf);
+		const new_buf = new Uint8Array(parent_id_size);
+		fill_random(new_buf, 0, parent_id_size);
+		return traceparent(this.version, this.trace_id, toHEX(new_buf), this.flags);
 	},
 
 	toString() {
 		return `${this.version}-${this.trace_id}-${this.parent_id}-${this.flags}`;
 	},
-});
+})
 
 export const make = (): Traceparent => {
-	const buf = new Uint8Array(26);
-	buf[v_offset] = 0; // version
-	buf[f_offset] = 0; // flags
-	fill_random(buf, t_offset, t_size); // trace-id
-	fill_random(buf, p_offset, p_size); // parent-id
-
-	return traceparent(buf);
+	const total_size = trace_id_size + parent_id_size
+	const buf = new Uint8Array(total_size);
+	fill_random(buf, 0, total_size);
+	return traceparent(
+		default_version,
+		toHEX(buf.slice(0, trace_id_size)),
+		toHEX(buf.slice(trace_id_size, total_size)),
+		sampled_flag);
 };
 
 export const parse = (value: string) => {
 	if (value.length > 55) return null;
-	const buf = new Uint8Array(26);
-
 	const segs = value.split('-');
-	buf.set(asHEX(segs[0]), v_offset); // version
-	buf.set(asHEX(segs[1]), t_offset); // trace-id
-	buf.set(asHEX(segs[2]), p_offset); // parent-id
-	buf.set(asHEX(segs[3]), f_offset); // flags
-
-	return traceparent(buf);
+	return traceparent(segs[0],segs[1],segs[2],segs[3]);
 };
