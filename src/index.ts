6cefe4cd8d9a833afe35f3b4c01e83458cc0216c
Marais Rossouw <me@marais.co>
2024-02-26 20:35:20 +1000

    chore: some formatting

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { random as r } from '@lukeed/csprng';
-import type { Traceparent } from 'tctx';
+import { random as r } from "@lukeed/csprng";
+import type { Traceparent } from "tctx";
 
 let IDX = 256,
 	HEX: string[] = [];
@@ -7,11 +7,11 @@ for (; IDX--; ) HEX[IDX] = (IDX + 256).toString(16).substring(1);
 
 function to_hex(arr: Uint8Array) {
 	let i = 0,
-		output = '';
+		output = "";
 	// @ts-ignore
 	for (; i < arr.length; i++) output += HEX[arr[i]];
 	return output;
-};
+}
 
 function random(size: number) {
 	return /*#__INLINE__*/ to_hex(r(size));
@@ -29,17 +29,12 @@ Anatomy of a Traceparent
 version (2 hex)
 */
 
-const W3C_TRACEPARENT_VERSION = '00';
+const W3C_TRACEPARENT_VERSION = "00";
 
 export const FLAG_SAMPLE = 0b00000001;
 export const FLAG_RANDOM = 0b00000010;
 
-function traceparent(
-	version: string,
-	trace_id: string,
-	parent_id: string,
-	flags: number,
-): Traceparent {
+function traceparent(version: string, trace_id: string, parent_id: string, flags: number): Traceparent {
 	return {
 		version,
 		trace_id,
@@ -47,34 +42,24 @@ function traceparent(
 		flags,
 
 		child() {
-			return traceparent(
-				this.version,
-				this.trace_id,
-				random(8),
-				this.flags & ~FLAG_SAMPLE,
-			);
+			return traceparent(this.version, this.trace_id, random(8), this.flags & ~FLAG_SAMPLE);
 		},
 
 		toString() {
-			let flags = this.flags.toString(16).padStart(2, '0');
+			let flags = this.flags.toString(16).padStart(2, "0");
 			return `${this.version}-${this.trace_id}-${this.parent_id}-${flags}`;
-		}
-	}
+		},
+	};
 }
 
 export function make() {
 	let id = random(24);
-	return traceparent(
-		W3C_TRACEPARENT_VERSION,
-		id.slice(0, 32),
-		id.slice(32),
-		FLAG_RANDOM,
-	);
+	return traceparent(W3C_TRACEPARENT_VERSION, id.slice(0, 32), id.slice(32), FLAG_RANDOM);
 }
 
 export function parse(value: string) {
 	if (value.length > 55) return null;
-	let segs = value.split('-');
+	let segs = value.split("-");
 	return traceparent(segs[0], segs[1], segs[2], parseInt(segs[3], 16));
 }
 
